7a8ba87b05200f06c6ba0a9056a8a37f15b6539c
Rodrigo <info@sapienlab.com>
2013-03-22 19:42:57 -0400

    Fixed multiple errors, now work fine

--- main.js
+++ main.js
@@ -2,77 +2,86 @@
 
 	define([], function() {
 
-		var ls = window.localStorage;
-
-		var hasLocalStorage = !!ls;
+		var ls = window.localStorage;					
 
 		var volatiles = {};
 
 		var Volatile = function(collectionName) {
 			this.collectionName = collectionName;
-			volatiles[collectionName] = this.index = {};
+			this.size = 0;
+			volatiles[collectionName] = this._index = {};			
 		};
 		Volatile.prototype = {
 			get : function(key) {
-				return this.index[key];
+				return this._index[key];
 			},
 			set : function(key, data) {
-				this.index[key] = data;
+				if(!this.has(key))
+					this.size++;  
+				this._index[key] = data;
 			},
 			remove : function(key) {
-				delete this.index[key];
+				if(this.has(key))
+					this.size--;
+				delete this._index[key];
+			},
+			has: function() {
+				return !! this._index[key];
 			},
 			forEach : function(iterator) {
-				for (var key in this.index) {
-					iterator(this.index[key], key);
+				for (var key in this._index) {
+					iterator(this._index[key], key);
 				};
 			},
 			size : function() {
-				return this.index.length;
+				return this._index.length;
 			},
-			destroy : function() {
+			_destroy : function() {				
 				delete volatiles[this.collectionName];
 			}
 		};
 
 		var Persistent = function(collectionName) {
 			this.collectionName = collectionName;
-			this.prefix = '__' + collectionName + '__';
-			this.countKey = '__' + collectionName + '-count__';
-			this.prefixRegExp = new RegExp('^' + this.prefix);
-			ls.getItem(this.countKey) || ls.setItem(this.countKey, 0);
+			this._prefix = '__' + collectionName + '__';
+			this._countKey = '__' + collectionName + '-count__';
+			this._prefixRegExp = new RegExp('^' + this._prefix);			
+			ls.getItem(this._countKey) || ls.setItem(this._countKey, 0);		
 		};
-		Persistent.prototype = {
-			JSON : JSON,
+		Persistent.prototype = {			
 			get : function(key) {
-				return this.JSON.parse(ls.getItem[this.prefix + key]);
+				return JSON.parse(ls.getItem(this._prefix + key));
 			},
 			set : function(key, data) {
 				if (!ls.getItem(key))
-					ls.setItem(this.countKey, ls.getItem(this.countKey) + 1);
-				ls.setItem(this.prefix + key, this.JSON.stringify(data));
+					ls.setItem(this._countKey, parseInt(ls.getItem(this._countKey)) + 1);
+				ls.setItem(this._prefix + key, JSON.stringify(data));
 			},
 			remove : function(key) {
-				if (ls.getItem(this.prefix + key))
-					ls.setItem(this.countKey, ls.getItem(this.countKey) - 1);
-				ls.removeItem(this.prefix + key);
+				if (ls.getItem(this._prefix + key))
+					ls.setItem(this._countKey, parseInt(ls.getItem(this._countKey)) - 1);
+				ls.removeItem(this._prefix + key);
+			},
+			has: function(key) {
+				return !!ls.getItem(this._prefix + key);
 			},
 			forEach : function(iterator) {
 				for (var i = 0; i < ls.length; i++) {
-					var key = ls.key(i);
-					if (this.prefixRegExp.test(key)) {
-						iterator(ls.getItem(key), key);
+					var keyCollection = ls.key(i), key;
+					if (this._prefixRegExp.test(keyCollection)) {
+						key = keyCollection.replace(this._prefixRegExp, '');				
+						iterator(this.get(key), key);
 					}
 				}
 			},
 			size : function() {
-				return ls.getItem(this.countKey);
+				return parseInt(ls.getItem(this._countKey));
 			},
-			destroy : function() {
-				ls.removeItem(this.countKey);
+			_destroy : function() {
+				ls.removeItem(this._countKey);
 				for (var i = 0; i < ls.length; i++) {
 					var key = ls.key(i);
-					if (this.prefixRegExp.test(key)) {
+					if (this._prefixRegExp.test(key)) {
 						ls.removeItem(key);
 					}
 				}
@@ -83,18 +92,18 @@
 			Volatile : Volatile,
 			Persistent : Persistent,
 			get : function(collectionName) {
-				return new (hasLocalStorage ? Persistent : Volatile)(collectionName);
+				return new (!!ls ? Persistent : Volatile)(collectionName);
 			},
 			remove : function(collectionName) {
-				this.get(collectionName).destroy();
+				this.get(collectionName)._destroy();
 			},
-			setJsonUtil : function(JSON) {
-				Persistent.prototye.JSON = JSON;
+			setJsonUtil : function(JSONUtil) {
+				JSON = JSONUtil;
 			}
 		};
 
 	});
 
-})( typeof define == "undefined" ? function(deps, factory) {
+})( typeof define == "undefined" || !define.amd ? function(deps, factory) {
 	lightDb = factory();
 } : define);
